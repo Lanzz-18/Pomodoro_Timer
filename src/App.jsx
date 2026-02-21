cf830f2c4199c13b21fa9c0b258a0f328e8db289
Senal Perera <senalperera777@gmail.com>
2026-02-22 04:23:08 +0530

    Added HTML structure + CSS styling

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,21 @@ import './App.css'
 
 function App() {
 
+  const workTab = document.getElementById("work")
+  const breakTab = document.getElementById("break")
+
   return (
     <>
-      
+      <div className="tab-group">
+        <p id="work">Work</p>
+        <p id="break">Break</p>
+      </div>
+      <div id="time-display"></div>
+      <div className="button-group">
+        <button>Start</button>
+        <button>Pause</button>
+        <button>Reset</button>
+      </div>
     </>
   )
 }
