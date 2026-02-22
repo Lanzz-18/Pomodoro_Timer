c5d7114ec73ed9e6e07f1bde255585571bfc85db
Senal Perera <senalperera777@gmail.com>
2026-02-22 05:44:32 +0530

    Made timer logic

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,45 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import './App.css'
 
 function App() {
+  // if work tab is pressed -> set timer to 25, if break is pressed set to 5. have work as default tab
+  const [isRunning, setIsRunning] = useState(false)
+  const [isWork, setIsWork] = useState(true)  
+  const [seconds, setSeconds] = useState(25 * 60) // Stores seconds count (25mins)
 
-  const workTab = document.getElementById("work")
-  const breakTab = document.getElementById("break")
+  const handleTab = (work) => {
+    setIsWork(work)
+    setIsRunning(false)
+    setSeconds(work ? 25 * 60 : 5 * 60)
+  }
+
+  useEffect(() => {
+    if(!isRunning){
+      return;
+    }
+    const timer = setInterval(() => {
+      setSeconds(prev => prev -1)
+    }, 1000)
+    return () => clearInterval(timer)
+  }, [isRunning])
+
+  const formatTime = (secs) => {
+    const minutes = Math.floor(secs / 60);
+    const seconds = secs % 60
+    return `${String(minutes).padStart(2, "0")}:${String(seconds).padStart(2, "0")}`
+  }
 
   return (
     <>
       <div className="tab-group">
-        <p id="work">Work</p>
-        <p id="break">Break</p>
+        <p id="work" onClick={() => handleTab(true)}>Work</p>
+        <p id="break" onClick={() => handleTab(false)}>Break</p>
       </div>
-      <div id="time-display"></div>
+      <div id="time-display">{formatTime(seconds)}</div>
       <div className="button-group">
-        <button>Start</button>
-        <button>Pause</button>
-        <button>Reset</button>
+        <button onClick={() => setIsRunning(true)}>Start</button>
+        <button onClick={() => setIsRunning(false)}>Pause</button>
+        <button onClick={() => {setIsRunning(false); setSeconds(isWork ? 25 * 60 : 5 * 60)}}>Reset</button>
       </div>
     </>
   )
